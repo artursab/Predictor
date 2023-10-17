1777b0cd1a61fb03eb727da93bc85472c35697b0
artursab <arturs.belavskis@gmail.com>
2023-10-17 14:43:20 +0200

    predicted

--- Predictor/Form1.cs
+++ Predictor/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,34 @@ namespace Predictor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private async void bPredict_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        progressBar1.Value = i;
+                    }));
+                    
+                    Thread.Sleep(100);
+                }
+            });
+
+            MessageBox.Show("predicted");
         }
     }
 }
