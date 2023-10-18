6e08c1ecafd0ccdbd169bfd581e0193bea35728b
artursab <arturs.belavskis@gmail.com>
2023-10-18 12:48:13 +0200

    pred

--- Predictor/Form1.cs
+++ Predictor/Form1.cs
@@ -13,6 +13,8 @@ namespace Predictor
 {
     public partial class Form1 : Form
     {
+        private const string APP_NAME = "ULTIMATE PREDICTOR"; 
+
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +22,7 @@ namespace Predictor
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            this.Text = APP_NAME;
         }
 
         private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
@@ -30,20 +32,41 @@ namespace Predictor
 
         private async void bPredict_Click(object sender, EventArgs e)
         {
+            bPredict.Enabled = false;
             await Task.Run(() =>
             {
                 for (int i = 1; i <= 100; i++)
                 {
                     this.Invoke(new Action(() =>
                     {
-                        progressBar1.Value = i;
+                        UpdateProgressBar(i);
+                        this.Text = $"{i}%";
                     }));
-                    
-                    Thread.Sleep(100);
+
+                    Thread.Sleep(10);
                 }
             });
 
             MessageBox.Show("predicted");
+
+            progressBar1.Value = 0;
+            this.Text = APP_NAME;
+            bPredict.Enabled = true;
+        }
+
+        private void UpdateProgressBar(int i)
+        {
+            if (i == progressBar1.Maximum)
+            {
+                progressBar1.Maximum = i + 1;
+                progressBar1.Value = i + 1;
+                progressBar1.Maximum = i;
+            }
+            else
+            {
+                progressBar1.Value = i + 1;
+            }
+            progressBar1.Value = i;
         }
     }
 }
